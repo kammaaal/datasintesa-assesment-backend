719bb29181ea31836875e243dafab661d8a1920e
kammaaal <kemalandraza@gmail.com>
2022-09-04 07:50:48 +0700

    erase unused filed and folder

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,14 +7,8 @@ router.get('/users', users.getAll);
 // GET localhost:4000/users/get/2 => get user data based on Userid
 router.get('/users/get/:id', users.getUserById);
 
-router.get('/form', function (req, res, next) {
-  res.render('users');
-});
-
 // POST localhost:4000/users/create => create user data
-router.post('/users/create', users.create,  function (req, res, next) {
-  res.redirect('/users/form');
-});
+router.post('/users/create', users.create);
 
 
 module.exports = router;
